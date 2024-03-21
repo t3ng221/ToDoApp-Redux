d629dad12177eec38672de7a4162be447cea172c
Md. Sazedur Rahman <sazedur15-2169@diu.edu.bd>
2024-03-21 23:43:55 +0600

    task list style changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,9 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { addTodo, removeAllToDo, removeToDo } from "./features/todo/todoSlice";
 import TextField from "@mui/material/TextField";
-import {
-  Button,
-  Container,
-  FormControl,
-  FormHelperText,
-  Grid,
-  Input,
-  InputLabel,
-} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Alert, Button, Container, FormControl, Grid } from "@mui/material";
 
 function App() {
   const todos = useSelector((state) => state.todos);
@@ -41,6 +35,7 @@ function App() {
         alignItems="center"
       >
         <FormControl>
+          <h1>Enter Task Your Task Details</h1>
           <TextField
             value={text}
             onChange={(e) => setText(e.target.value)}
@@ -52,34 +47,45 @@ function App() {
             onClick={() => addTodoHandler()}
             variant="contained"
           >
-            Add ToDo
+            Add Task
           </Button>
         </FormControl>
+
+        <Container container>
+          <ul>
+            {todos.length !== 0 ? (
+              todos.map((e) => (
+                <li key={e.id}>
+                  <Alert variant="outlined" severity="info">
+                    <h2 className="mt-10">
+                      {e.text}{" "}
+                      <IconButton
+                        aria-label="delete"
+                        size="large"
+                        onClick={() => dispatch(removeToDo({ id: e.id }))}
+                        variant="contained"
+                      >
+                        <DeleteIcon fontSize="inherit" />
+                      </IconButton>
+                    </h2>
+                  </Alert>
+                </li>
+              ))
+            ) : (
+              <h2>No Task Available</h2>
+            )}
+          </ul>
+          <Button
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            onClick={() => dispatch(removeAllToDo())}
+            variant="contained"
+          >
+            Delete All
+          </Button>
+        </Container>
       </Grid>
-      <Container fixed>
-        <ul>
-          {todos.length !== 0 ? (
-            todos.map((e) => (
-              <li key={e.id}>
-                <h2>
-                  {e.text}{" "}
-                  <Button
-                    onClick={() => dispatch(removeToDo({ id: e.id }))}
-                    variant="contained"
-                  >
-                    Delete
-                  </Button>
-                </h2>
-              </li>
-            ))
-          ) : (
-            <h2>No Task Available</h2>
-          )}
-        </ul>
-        <Button onClick={() => dispatch(removeAllToDo())} variant="contained">
-          Delete All
-        </Button>
-      </Container>
     </>
   );
 }
